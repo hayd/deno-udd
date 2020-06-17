6e886b6ceb33dbf0965d64f9bfa5722793d30e72
Andy Hayden <andyhayden1@gmail.com>
2020-06-17 00:01:18 -0700

    Update deno to 1.1.0 and std to 0.57.0

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export {
   parse as parseArgs,
-} from "https://deno.land/std@v0.50.0/flags/mod.ts";
-export * as colors from "https://deno.land/std@v0.50.0/fmt/colors.ts";
-export { decode, encode } from "https://deno.land/std@v0.50.0/encoding/utf8.ts";
+} from "https://deno.land/std@v0.57.0/flags/mod.ts";
+export * as colors from "https://deno.land/std@v0.57.0/fmt/colors.ts";
+export { decode, encode } from "https://deno.land/std@v0.57.0/encoding/utf8.ts";
