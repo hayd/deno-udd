5151b0a06bcc889de1a7d7bea5bebec6445fbf47
GitHub <noreply@github.com>
2022-07-31 15:34:28 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { parse as parseArgs } from "https://deno.land/std@0.149.0/flags/mod.ts";
-export { expandGlob } from "https://deno.land/std@0.149.0/fs/mod.ts";
-export * as colors from "https://deno.land/std@0.149.0/fmt/colors.ts";
+export { parse as parseArgs } from "https://deno.land/std@0.150.0/flags/mod.ts";
+export { expandGlob } from "https://deno.land/std@0.150.0/fs/mod.ts";
+export * as colors from "https://deno.land/std@0.150.0/fmt/colors.ts";
